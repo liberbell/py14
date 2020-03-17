ac0d2909fb40c7c1c79c9fcf0433a5cc9dc05005
Liberbell <liberbell@gmail.com>
2020-03-17 17:28:18 +0900

    multipules1

--- multipules.py
+++ multipules.py
@@ -5,3 +5,8 @@ print("I am a single quated string.")
 print("Python " + 'is' + """ Awesome""")
 
 print('Python ' * 3)
+
+quetion = "What did you have for lunch?"
+print(quetion)
+answer = input()
+print("You had " + answer + " That sound delicious!")
