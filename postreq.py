5d22dbcd342e9d68123a7de9af9203a24a46d51a
Liberbell <liberbell@gmail.com>
2020-03-20 12:20:09 +0900

    postreq5

--- postreq.py
+++ postreq.py
@@ -2,6 +2,6 @@ import requests
 from pprint import pprint
 import webbrowser
 
-r_post = requests.post("https://en.wikipedia.org/w/index.php", data={'search' : 'george harison'})
+r_post = requests.post("https://en.wikipedia.org/w/index.php", data={'search' : 'george harrison'})
 print(r_post.status_code)
-print(webbrowser.open(r_post))
+print(webbrowser.open(r_post.url))
