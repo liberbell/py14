367b59b1d8d5a2ab6f12fad3251a08594f13e880
Liberbell <liberbell@gmail.com>
2020-03-20 12:19:19 +0900

    postreq4

--- postreq.py
+++ postreq.py
@@ -1,5 +1,7 @@
 import requests
 from pprint import pprint
+import webbrowser
 
 r_post = requests.post("https://en.wikipedia.org/w/index.php", data={'search' : 'george harison'})
 print(r_post.status_code)
+print(webbrowser.open(r_post))
