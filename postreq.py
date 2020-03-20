e5093a7cafd83cfda0b7b4a6d5da96a09e76c630
Liberbell <liberbell@gmail.com>
2020-03-20 12:24:30 +0900

    postreq7

--- postreq.py
+++ postreq.py
@@ -5,4 +5,5 @@ import webbrowser
 r_post = requests.post("https://en.wikipedia.org/w/index.php", data={'search' : 'george harrison'})
 print(r_post.status_code)
 print(type(r_post))
-print(webbrowser.open(r_post.url))
+pprint(r_post.text)
+# print(webbrowser.open(r_post.url))
