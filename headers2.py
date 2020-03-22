78e7cfff2e784248a7392411ab5b99b0077d5307
Liberbell <liberbell@gmail.com>
2020-03-22 16:13:02 +0900

    headers8

--- headers2.py
+++ headers2.py
@@ -13,3 +13,5 @@ resp_obj = requests.get("https://en.wikipedia.org/wiki/Monty_Python")
 print(resp_obj.headers)
 # print(webbrowser.open(resp_obj.url))
 print(resp_obj.request.headers)
+
+r = requests.get("https://httpbin.org/user-agent", headers = {'User-Agent': 'Internet Explorer/2.0'})
