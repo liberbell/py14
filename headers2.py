c01718a47e38e85266f0aef666269b19a3f384bf
Liberbell <liberbell@gmail.com>
2020-03-22 16:05:24 +0900

    headers2-4

--- headers2.py
+++ headers2.py
@@ -5,3 +5,5 @@ url = "https://swapi.co/api/people/3"
 headers = {'user-agent': 'Google Chrome'}
 resp = requests.get(url, headers=headers)
 print(resp.status_code)
+print(resp.headers)
+print(resp.headers['content-type'])
