bee0de168972694bfb76740cf34afff89fb8f860
Liberbell <liberbell@gmail.com>
2020-03-22 16:02:50 +0900

    headers2-3

--- headers2.py
+++ headers2.py
@@ -1,3 +1,7 @@
 import requests
 from pprint import pprint
 
+url = "https://swapi.co/api/people/3"
+headers = {'user-agent': 'Google Chrome'}
+resp = requests.get(url, headers=headers)
+print(resp.status_code)
