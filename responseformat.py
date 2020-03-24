e66cfb9fe96621c3e87840987006585f343f80b3
Liberbell <liberbell@gmail.com>
2020-03-24 14:07:27 +0900

    responseformat5

--- responseformat.py
+++ responseformat.py
@@ -2,5 +2,5 @@ import requests
 from pprint import pprint
 import json
 
-resp = requests.get("https://swapi.co/api/vehcles/4/")
+resp = requests.get("https://swapi.co/api/vehicles/4/")
 print(resp.status_code)
