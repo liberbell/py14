7beae210fa334d063eb438a83f4d668b2638cd27
Liberbell <liberbell@gmail.com>
2020-03-24 17:45:18 +0900

    responseformat9

--- responseformat.py
+++ responseformat.py
@@ -6,3 +6,6 @@ resp_obj = requests.get("https://swapi.co/api/vehicles/4/")
 print(resp_obj.status_code)
 # pprint(resp_obj.json())
 print(resp_obj.headers['content-type'])
+
+resp = requests.get("https://www.yahoo.co.jp")
+pprint(resp.json())
