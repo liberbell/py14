a799cf6bcad944379087f1201af0bf6e369dbdee
Liberbell <liberbell@gmail.com>
2020-03-25 14:04:49 +0900

    redirecttimeout5

--- redirecttimeout.py
+++ redirecttimeout.py
@@ -2,11 +2,15 @@ import requests
 from pprint import pprint
 
 response = requests.get("https://gmail.com")
-print(response.history)
-print(response.url)
+# print(response.history)
+# print(response.url)
 
 if response.history:
     print("Redirect history:")
     for resp in response.history:
         print(resp.status_code, resp.url)
     print("\nFinal destination:")
+    print(response.status_code, response.url)
+
+else:
+    print("Request was not redirect.")
